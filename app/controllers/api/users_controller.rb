8500c6db324ca3e6f6ee67dc4fe4cb40f4be99bb
randyjap <randyjap@gmail.com>
2017-01-30 19:43:48 -0800

    Twilio with mobile app

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,32 +1,23 @@
 class Api::UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token
 
   def create
-    account_sid = ENV["twilio_account_sid"]
-    auth_token = ENV["twilio_auth_token"]
-    from = ENV["twilio_phone_number"]
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
-    @client.account.messages.create({
-        :to => params[:user][:to],
-        :from => from,
-        :body => params[:user][:message],
-    })
-
-    render json: "got it!", status: 200
-
-    # @user = User.new(user_params)
-    # if @user.save
-    #   session[:user_id] = @user.id
-    #   authy = Authy::API.register_user(
-    #     cellphone: @user.phone_number,
-    #     country_code: @user.country_code,
-    #     email: "test@example.com"
-    #   )
-    #   @user.update(authy_id: authy.id)
-    #   render json: @user, status: 200
-    # else
-    #   render json: @user.errors.full_messages, status: 422
-    # end
+    @user = User.new(user_params)
+    if @user.save
+      account_sid = ENV["twilio_account_sid"]
+      auth_token = ENV["twilio_auth_token"]
+      to = "#{@user.country_code} #{user.phone_number}"
+      from = ENV["twilio_phone_number"]
+      message = "Please enter verification code #{@user.authy_id}"
+      @client = Twilio::REST::Client.new(account_sid, auth_token)
+      @client.account.messages.create({
+          :to => to,
+          :from => from,
+          :body => message
+      })
+      render json: @user, status: 200
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   private
