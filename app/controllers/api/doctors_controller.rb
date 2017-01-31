9a80b61788c600ff3e26939c868da17b91f906ff
randyjap <randyjap@gmail.com>
2017-01-30 20:27:15 -0800

    Secure doctor page

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -10,10 +10,14 @@ class Api::DoctorsController < ApplicationController
   end
 
   def show
-    @user = User.first
-    @doctor = Doctor.find(params[:id])
-    @doctor_schedule = @doctor.timeslots(Date.today)
-    @doctor_schedule_2 = @doctor.timeslots(Date.today + 1)
-    @doctor_schedule_3 = @doctor.timeslots(Date.today + 2)
+    if current_user
+      @user = User.first
+      @doctor = Doctor.find(params[:id])
+      @doctor_schedule = @doctor.timeslots(Date.today)
+      @doctor_schedule_2 = @doctor.timeslots(Date.today + 1)
+      @doctor_schedule_3 = @doctor.timeslots(Date.today + 2)
+    else
+      render json: "You are not authorized", status: 401
+    end
   end
 end
