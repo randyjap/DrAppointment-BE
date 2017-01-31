225c73509cf525ebcaf95135784c3324dadc3f75
randyjap <randyjap@gmail.com>
2017-01-31 11:39:18 -0800

    Added create method in appointment controller

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,30 +1,32 @@
 class Api::AppointmentsController < ApplicationController
-  # def create
-  #   doctor = Doctor.find(params[:appointment][:doctor_id]),
-  #   user = User.find(params[:appointment][:user_id]),
-  #   time_slot = TimeSlot.find(params[:appointment][:time_slot_id])
-  #
-  #   @appointment = Appointment.new(
-  #     doctor: doctor,
-  #     user: user,
-  #     time_slot: time_slot
-  #   )
-  #
-  #   if @appointment.save
-  #     account_sid = ENV["twilio_account_sid"]
-  #     auth_token = ENV["twilio_auth_token"]
-  #     to = "1 #{@user.phone_number}"
-  #     from = ENV["twilio_phone_number"]
-  #     message = "Your appointment is confirmed at"
-  #     @client = Twilio::REST::Client.new(account_sid, auth_token)
-  #     @client.account.messages.create({
-  #         :to => to,
-  #         :from => from,
-  #         :body => message
-  #     })
-  #     render json: @user, status: 200
-  #   else
-  #     render json: @user.errors.full_messages, status: 422
-  #   end
-  # end
+  def create
+    doctor = Doctor.find(params[:appointment][:doctor_id]),
+    user = User.find(params[:appointment][:user_id]),
+    time_slot = TimeSlot.find(params[:appointment][:time_slot_id])
+
+    @appointment = Appointment.new(
+      doctor: doctor,
+      user: user,
+      time_slot: time_slot
+    )
+
+    if @appointment.save
+      account_sid = ENV["twilio_account_sid"]
+      auth_token = ENV["twilio_auth_token"]
+      to = "1 #{user.phone_number}"
+      from = ENV["twilio_phone_number"]
+      doctor_name = "#{doctor.salutation} #{doctor.first_name} #{doctor.last_name}"
+
+      message = "Your appointment is confirmed at #{@appointment.time_slot.time} with #{doctor_name}"
+      @client = Twilio::REST::Client.new(account_sid, auth_token)
+      @client.account.messages.create({
+          :to => to,
+          :from => from,
+          :body => message
+      })
+      render json: @appointment, status: 200
+    else
+      render json: @appointment.errors.full_messages, status: 422
+    end
+  end
 end
