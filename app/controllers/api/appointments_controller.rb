e30db5b5f1a68dc778f57953db6e5fead148789d
randyjap <randyjap@gmail.com>
2017-01-31 11:33:58 -0800

    Disable appointments

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,30 +1,30 @@
 class Api::AppointmentsController < ApplicationController
-  def create
-    doctor = Doctor.find(params[:appointment][:doctor_id]),
-    user = User.find(params[:appointment][:user_id]),
-    time_slot = TimeSlot.find(params[:appointment][:time_slot_id])
-
-    @appointment = Appointment.new(
-      doctor: doctor,
-      user: user,
-      time_slot: time_slot
-    )
-
-    if @appointment.save
-      account_sid = ENV["twilio_account_sid"]
-      auth_token = ENV["twilio_auth_token"]
-      to = "1 #{@user.phone_number}"
-      from = ENV["twilio_phone_number"]
-      message = "Your appointment is confirmed at"
-      @client = Twilio::REST::Client.new(account_sid, auth_token)
-      @client.account.messages.create({
-          :to => to,
-          :from => from,
-          :body => message
-      })
-      render json: @user, status: 200
-    else
-      render json: @user.errors.full_messages, status: 422
-    end
-  end
+  # def create
+  #   doctor = Doctor.find(params[:appointment][:doctor_id]),
+  #   user = User.find(params[:appointment][:user_id]),
+  #   time_slot = TimeSlot.find(params[:appointment][:time_slot_id])
+  #
+  #   @appointment = Appointment.new(
+  #     doctor: doctor,
+  #     user: user,
+  #     time_slot: time_slot
+  #   )
+  #
+  #   if @appointment.save
+  #     account_sid = ENV["twilio_account_sid"]
+  #     auth_token = ENV["twilio_auth_token"]
+  #     to = "1 #{@user.phone_number}"
+  #     from = ENV["twilio_phone_number"]
+  #     message = "Your appointment is confirmed at"
+  #     @client = Twilio::REST::Client.new(account_sid, auth_token)
+  #     @client.account.messages.create({
+  #         :to => to,
+  #         :from => from,
+  #         :body => message
+  #     })
+  #     render json: @user, status: 200
+  #   else
+  #     render json: @user.errors.full_messages, status: 422
+  #   end
+  # end
 end
