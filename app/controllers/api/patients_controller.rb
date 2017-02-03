32ec953f04b7cff31004a57c63b931d41d903046
randyjap <randyjap@gmail.com>
2017-02-02 23:09:14 -0800

    Patient controller rights

--- app/controllers/api/patients_controller.rb
+++ app/controllers/api/patients_controller.rb
@@ -1,11 +1,22 @@
 class Api::PatientsController < ApplicationController
-  def create
+  skip_before_action :verify_authenticity_token
 
+  def create
+    patient = Patient.new(
+      first_name: params[:patient][:firstName],
+      last_name: params[:patient][:lastName],
+      user: current_user
+    )
+    if patient.save
+      render json: patient, status: 200
+    else
+      render status: 401
+    end
   end
 
   def index
-    # @patients = current_user.patients
-    @patients = User.find(1).patients
+    @patients = current_user.patients
+    # @patients = User.find(1).patients
   end
 
   def destroy
