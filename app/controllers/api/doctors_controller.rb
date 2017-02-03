10dae289cf39ae8e12c509c1fbbc6c7385adbafa
Raymond Lee <raylee0525@gmail.com>
2017-02-03 00:44:24 -0800

    doctors controller fixed

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -27,7 +27,7 @@ class Api::DoctorsController < ApplicationController
   end
 
   def show
-    if @user
+    if @user = User.first
       @doctor = Doctor.find(params[:id])
       @doctor_schedule = @doctor.timeslots(Date.today)
       @doctor_schedule_2 = @doctor.timeslots(Date.today + 1)
