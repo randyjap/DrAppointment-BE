c70fd5571da727e017c95cdfb93b2da3ce7a1ed8
randyjap <randyjap@gmail.com>
2017-02-05 11:17:17 -0800

    Validate against duplicate patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,4 +5,15 @@ class Patient < ApplicationRecord
   has_many :appointment_patients
   has_many :appointments, through: :appointment_patients
   has_many :doctors, through: :appointments
+
+  validate :unique_to_user
+
+  def unique_to_user
+    duplicate = self.user.patients.any? do |patient|
+      patient.first_name == self.first_name &&
+      patient.last_name == self.last_name
+    end
+
+    errors.add(:patient, "has already been created") if duplicate
+  end
 end
